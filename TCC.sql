eb48baff134a0e6d0b9a8a1162a7bdb1128ecb0d
PedroRocha03 <161655186+PedroRocha03@users.noreply.github.com>
2024-05-22 09:00:34 -0300

    Update TCC.sql

--- TCC.sql
+++ TCC.sql
@@ -4,9 +4,3 @@ insert into TCC (id_tcc, prof_id, titulo) values ('5624', 539191469, 'Reconhecim
 insert into TCC (id_tcc, prof_id, titulo) values ('9410', 683936993, 'Análise de Redes Sociais com Data Mining');
 insert into TCC (id_tcc, prof_id, titulo) values ('3856', 816398345, 'Processamento de Linguagem Natural');
 insert into TCC (id_tcc, prof_id, titulo) values ('6732', 678591153, 'Desenvolvimento de Aplicativos com React Native');
-insert into TCC (id_tcc, prof_id, titulo) values ('1298', 292847945, 'Sistemas de Apoio à Decisão');
-insert into TCC (id_tcc, prof_id, titulo) values ('5073', 399664635, 'Robótica Educacional para Crianças');
-insert into TCC (id_tcc, prof_id, titulo) values ('8462', 332515345, 'Desenvolvimento de Sistemas para IoT');
-insert into TCC (id_tcc, prof_id, titulo) values ('4139', 778873933, 'Segurança da Informação em Empresas');
-insert into TCC (id_tcc, prof_id, titulo) values ('6025', 121854349, 'Aplicações de Inteligência Artificial em Saúde');
-insert into TCC (id_tcc, prof_id, titulo) values ('8743', 211513559, 'Desenvolvimento de APIs RESTful');
