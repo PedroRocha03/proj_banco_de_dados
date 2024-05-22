00eb4b1b28749137859590a65c7c54af1727b4d9
PedroRocha03 <161655186+PedroRocha03@users.noreply.github.com>
2024-05-22 12:35:38 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -9,3 +9,19 @@ FROM
     JOIN Disciplinas d ON dm.id_disc = d.id_disc
 WHERE
     dm.prof_id = "insira o id do professor";
+
+-- Grupo de TCC de tais alunos e seu professor orientador
+
+SELECT
+    t.id_tcc,
+    t.titulo,
+    p.nome AS nome_orientador,
+    a.aluno_id,
+    a.nome AS nome_aluno
+FROM
+    TCC t
+    JOIN Grupo_TCC ta ON t.id_tcc = ta.id_tcc
+    JOIN Aluno a ON ta.aluno_id = a.aluno_id
+    JOIN Professor p ON t.orientador_id = p.prof_id
+WHERE
+    t.id_tcc = "insira o id do tcc";
