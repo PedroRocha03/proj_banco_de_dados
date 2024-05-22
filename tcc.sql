77a96cb034d9106c334648e3483eb1e7b1fab09a
PedroRocha03 <161655186+PedroRocha03@users.noreply.github.com>
2024-05-22 12:21:31 -0300

    Update tcc.sql

--- tcc.sql
+++ tcc.sql
@@ -1,6 +1,6 @@
-insert into TCC (id_tcc, prof_id, titulo) values ('7391', 748146844, 'Aplicações de Biometria em Segurança');
-insert into TCC (id_tcc, prof_id, titulo) values ('2087', 377276899, 'Desenvolvimento de Sistemas Embarcados');
-insert into TCC (id_tcc, prof_id, titulo) values ('5624', 539191469, 'Reconhecimento Facial com Deep Learning');
-insert into TCC (id_tcc, prof_id, titulo) values ('9410', 683936993, 'Análise de Redes Sociais com Data Mining');
-insert into TCC (id_tcc, prof_id, titulo) values ('3856', 816398345, 'Processamento de Linguagem Natural');
-insert into TCC (id_tcc, prof_id, titulo) values ('6732', 678591153, 'Desenvolvimento de Aplicativos com React Native');
+insert into TCC (id_tcc, orientador_id, titulo) values ('7391', 748146844, 'Aplicações de Biometria em Segurança');
+insert into TCC (id_tcc, orientador_id, titulo) values ('2087', 377276899, 'Desenvolvimento de Sistemas Embarcados');
+insert into TCC (id_tcc, orientador_id, titulo) values ('5624', 539191469, 'Reconhecimento Facial com Deep Learning');
+insert into TCC (id_tcc, orientador_id, titulo) values ('9410', 683936993, 'Análise de Redes Sociais com Data Mining');
+insert into TCC (id_tcc, orientador_id, titulo) values ('3856', 816398345, 'Processamento de Linguagem Natural');
+insert into TCC (id_tcc, orientador_id, titulo) values ('6732', 678591153, 'Desenvolvimento de Aplicativos com React Native');
