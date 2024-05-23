b51915995d4ef84935bc36f150ce3b3d8c2c3787
PedroRocha03 <pedroaugusto3003@gmail.com>
2024-05-22 21:46:46 -0300

    Deletar arquivos desnecessarios

--- curso.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO Curso (id_curso, nome_curso, id_dept) VALUES ('CIBa100', 'Bacharelado em Ciência da Computação', 'CI');
-INSERT INTO Curso (id_curso, nome_curso, id_dept) VALUES ('MAAn117', 'Análise Matemática Avançada', 'MA');
-INSERT INTO Curso (id_curso, nome_curso, id_dept) VALUES ('LELe119', 'Letras - Português e Inglês', 'LE');
-INSERT INTO Curso (id_curso, nome_curso, id_dept) VALUES ('ADAd129', 'Administração de Empresas', 'AS');
-INSERT INTO Curso (id_curso, nome_curso, id_dept) VALUES ('ENEn156', 'Engenharia Civil', 'EN');
-
-
