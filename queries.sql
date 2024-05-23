6f4cac53574656d355fbb450707d8a4187f081ac
PedroRocha03 <161655186+PedroRocha03@users.noreply.github.com>
2024-05-23 10:05:59 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -22,6 +22,33 @@ FROM
 WHERE
     dm.prof_id = 'insira o id do professor';
 
+-- Alunos que se formaram com todos as disciplinas da matriz curricular feitas, e com nota >= 5
+
+SELECT
+  A.nome AS nome_aluno,
+  A.aluno_id AS id_aluno,
+  MAX(H.ano) AS ano_formatura,
+  MAX(H.semestre) AS semestre_formatura
+FROM
+  Aluno A
+JOIN Historico H ON A.aluno_id = H.aluno_id
+WHERE
+  NOT EXISTS (
+    SELECT 1
+    FROM Matriz M
+    WHERE M.id_curso = A.id_curso
+    AND NOT EXISTS (
+      SELECT 1
+      FROM Historico H2
+      WHERE H2.aluno_id = A.aluno_id
+      AND H2.id_disc = M.id_disc
+      AND H2.nota >= 5
+    )
+  )
+GROUP BY
+  A.nome,
+  A.aluno_id;
+
 -- Professores que são chefes de um departamento e seus respectivos nomes
 
 SELECT P.nome AS nome_professor, D.nome_dept AS nome_departamento
